ba18011c284f8efa4207c5aa4f1754ee9f0eed45
alexliao95311 <alexliao365@gmail.com>
2021-04-28 13:19:44 -0700

    sync command used

--- rpg.py
+++ rpg.py
@@ -110,16 +110,3 @@ while(p.health > 0):
     if not commandFound:
       print "%s doesn't understand the suggestion." % p.name
  
-"""
-Copyright 2010 Francesco Balducci
- 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
- 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-""" 
